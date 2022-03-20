d43f155b452d76f8b9e6e67c73c0fb95518a710c
khattakdev <akkhattak65@gmail.com>
2022-03-20 05:29:16 +0000

    Deploying to gh-pages from @ khattakdev/my-appp2@6ab8adcd85b60f00016edfa8591ca3e6edc49cd0 🚀

--- server/chunks/675.js
+++ server/chunks/675.js
@@ -98,7 +98,7 @@ function _objectWithoutPropertiesLoose(source, excluded) {
     }
     return target;
 }
-const configEnv = {"deviceSizes":[640,750,828,1080,1200,1920,2048,3840],"imageSizes":[16,32,48,64,96,128,256,384],"path":"/_next/image","loader":"default"};
+const configEnv = {"deviceSizes":[640,750,828,1080,1200,1920,2048,3840],"imageSizes":[16,32,48,64,96,128,256,384],"path":"./","loader":"akamai"};
 const loadedImageURLs = new Set();
 const allImgs = new Map();
 let perfObserver;
